35067e4459922a35a006d8087bd1ccd1d91abe26
zzshan16 <zzshan16@hotmail.com>
2022-12-02 05:32:20 +0000

    day2

---
+++ day2a.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <math.h>
+#include "split.h"
+int main(){
+  FILE* fp = fopen("input", "r");
+  char buffer[32768];
+  int x = 0;
+  while (fgets(buffer, 32768, fp)){
+    *(buffer + strlen(buffer) -1) = '\0';//remove '\n' character
+    if (!strcmp(buffer, "A X")){
+      x += 1;
+      x += 3;
+      continue;// 4;
+    }
+    if (!strcmp(buffer, "A Y")){
+      x += 2;
+      x += 6;
+      continue;
+    }
+    if (!strcmp(buffer, "A Z")){
+      x += 3;
+      x +=0;
+      continue;
+    }
+    if (!strcmp(buffer, "B X")){
+      x += 1;
+      x += 0;
+    }
+    if (!strcmp(buffer, "B Y")){
+      x += 2;
+      x += 3;
+    }
+    if (!strcmp(buffer, "B Z")){
+      x += 3;
+      x += 6;
+    }
+    if (!strcmp(buffer, "C X")){
+      x+= 1;
+      x+= 6;
+    }
+    if (!strcmp(buffer, "C Y")){
+      x += 2;
+      x += 0;
+    }
+    if (!strcmp(buffer, "C Z")){
+      x += 3;
+      x += 3;
+    }
+  }
+  fclose(fp);
+  printf(" x = %d\n", x);
+return 0;
+}
